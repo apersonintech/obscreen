b4e3e71a30a389ba607b481c7e07e11f57054442
jr-k <jessym.reziga@gmail.com>
2024-02-27 15:16:34 +0100

    add restart logic

--- src/controller/SysinfoController.py
+++ src/controller/SysinfoController.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import os
+import sys
+
+from flask import Flask, render_template, jsonify
 from src.utils import get_ip_address
 
 
@@ -11,6 +14,7 @@ class SysinfoController:
 
     def register(self):
         self._app.add_url_rule('/sysinfo', 'sysinfo_attribute_list', self.sysinfo, methods=['GET'])
+        self._app.add_url_rule('/sysinfo/restart', 'sysinfo_restart', self.sysinfo_restart, methods=['POST'])
 
     def sysinfo(self):
         return render_template(
@@ -18,3 +22,9 @@ class SysinfoController:
             ipaddr=get_ip_address(),
             l=self._lang_dict,
         )
+
+    def sysinfo_restart(self):
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+        return jsonify({'status': 'ok'})
+
