e51061c984180de08896cdbc3124ff54d87a41c9
jr-k <jessym.reziga@gmail.com>
2024-02-27 11:07:31 +0100

    some final cleaning

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 
-def str_to_enum(strval: str, enum_class):
+def str_to_enum(str_val: str, enum_class):
     for enum_item in enum_class:
-        if enum_item.value == strval:
+        if enum_item.value == str_val:
             return enum_item
-    raise ValueError(f"{strval} is not a valid {enum_class.__name__} item")
+    raise ValueError(f"{str_val} is not a valid {enum_class.__name__} item")
