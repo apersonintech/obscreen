604ffda4af424d6ad64bd79e938f17b5efc2ebe6
jr-k <jessym.reziga@gmail.com>
2024-02-28 17:38:29 +0100

    some fixes + type variables

--- src/model/Variable.py
+++ src/model/Variable.py
@@ -1,15 +1,19 @@
 import json
+import time
 
 from typing import Optional, Union
-
+from src.model.VariableType import VariableType
+from src.utils import str_to_enum
 
 class Variable:
 
-    def __init__(self, name: str = '', description: str = '', value: Union[int, bool, str] = '', id: Optional[str] = None):
+    def __init__(self, name: str = '', description: str = '', type: Union[VariableType, str] = VariableType.STRING, value: Union[int, bool, str] = '', editable: bool = True, id: Optional[str] = None):
         self._id = id if id else None
         self._name = name
+        self._type = str_to_enum(type, VariableType) if isinstance(type, str) else type
         self._description = description
         self._value = value
+        self._editable = editable
 
     @property
     def id(self) -> Union[int, str]:
@@ -24,12 +28,28 @@ class Variable:
         self._name = value
 
     @property
+    def type(self) -> VariableType:
+        return self._type
+
+    @type.setter
+    def type(self, value: VariableType):
+        self._type = value
+
+    @property
     def description(self) -> str:
         return self._description
 
     @description.setter
     def description(self, value: str):
         self._description = value
+
+    @property
+    def editable(self) -> bool:
+        return self._editable
+
+    @editable.setter
+    def editable(self, value: bool):
+        self._editable = value
 
     @property
     def value(self) -> Union[int, bool, str]:
@@ -44,7 +64,9 @@ class Variable:
                f"id='{self.id}',\n" \
                f"name='{self.name}',\n" \
                f"value='{self.value}',\n" \
+               f"type='{self.type}',\n" \
                f"description='{self.description}',\n" \
+               f"editable='{self.editable}',\n" \
                f")"
 
     def to_json(self) -> str:
@@ -55,14 +77,30 @@ class Variable:
             "id": self.id,
             "name": self.name,
             "value": self.value,
+            "type": self.type.value,
             "description": self.description,
+            "editable": self.editable,
         }
 
-    def as_bool(self):
+    def as_bool(self) -> bool:
         return bool(int(self._value))
 
-    def as_string(self):
+    def as_string(self) -> str:
         return str(self._value)
 
-    def as_int(self):
+    def as_int(self) -> int:
         return int(self._value)
+
+    def as_ctime(self):
+        return time.ctime(self._value)
+
+    def display(self):
+        if self.type == VariableType.INT:
+            return self.as_int()
+        elif self.type == VariableType.BOOL:
+            return self.as_bool()
+        elif self.type == VariableType.TIMESTAMP:
+            return self.as_ctime()
+
+        return self.as_string()
+
---
+++ src/model/VariableType.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class VariableType(Enum):
+
+    BOOL = 'bool'
+    STRING = 'string'
+    INT = 'int'
+    TIMESTAMP = 'timestamp'
