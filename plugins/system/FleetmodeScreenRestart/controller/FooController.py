550c0433720997bc386d7fe10ffd8884a3c3bacd
jr-k <jessym.reziga@gmail.com>
2024-03-01 13:06:20 +0100

    add functional hook

--- plugins/system/FleetmodeScreenRestart/controller/FooController.py
+++ plugins/system/FleetmodeScreenRestart/controller/FooController.py
@@ -2,19 +2,18 @@ from flask import Flask, jsonify
 from src.interface.ObController import ObController
 
 
-class FooController(ObController):
+# class FooController(ObController):
 
-    def register(self):
-        self._app.add_url_rule('/foo', 'foo', self.foo, methods=['GET'])
-        self._app.add_url_rule('/foo_html', 'foo_html', self.foo_html, methods=['GET'])
-
-    def foo(self):
-        return jsonify({'foo': True})
-
-    def foo_html(self):
-        return self.plugin().render_view(
-            '@foo_html.jinja.html',
-            page_name="FOO"
-        )
-
-
+    # def register(self):
+    #     self._app.add_url_rule('/foo', 'foo', self.foo, methods=['GET'])
+    #     self._app.add_url_rule('/foo_html', 'foo_html', self.foo_html, methods=['GET'])
+    #
+    # def foo(self):
+    #     return jsonify({'foo': True})
+    #
+    # def foo_html(self):
+    #     return self.plugin().render_view(
+    #         '@foo_html.jinja.html',
+    #         page_name="FOO"
+    #     )
+    #
