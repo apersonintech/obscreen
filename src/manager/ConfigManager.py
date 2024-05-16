67673ddc71e2f46b9df3e6a5b6c1659e310d3a39
jr-k <jessym.reziga@gmail.com>
2024-05-16 16:12:40 +0200

    dyn user scripts

--- src/manager/ConfigManager.py
+++ src/manager/ConfigManager.py
@@ -135,9 +135,9 @@ xset s noblank
 unclutter -display :0 -noevents -grab &
 
 # Modify Chromium preferences to avoid restore messages
-mkdir -p /home/pi/.config/chromium/Default 2>/dev/null
-touch /home/pi/.config/chromium/Default/Preferences
-sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' /home/pi/.config/chromium/Default/Preferences
+mkdir -p __HOME__/.config/chromium/Default 2>/dev/null
+touch __HOME__/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' __HOME__/.config/chromium/Default/Preferences
 
 RESOLUTION=$(DISPLAY=:0 xrandr | grep '*' | awk '{print $1}')
 WIDTH=$(echo $RESOLUTION | cut -d 'x' -f 1)
@@ -145,6 +145,6 @@ HEIGHT=$(echo $RESOLUTION | cut -d 'x' -f 2)
 
 # Start Chromium in kiosk mode
 chromium-browser --disable-features=Translate --ignore-certificate-errors --disable-web-security --disable-restore-session-state --autoplay-policy=no-user-gesture-required --start-maximized --allow-running-insecure-content --remember-cert-error-decisions --noerrdialogs --kiosk --incognito --window-position=0,0 --window-size=${WIDTH},${HEIGHT} --display=:0 __PLAYER_URL__
-        """.replace('__PLAYER_URL__', player_url)
+        """.replace('__PLAYER_URL__', player_url).replace('__HOME__', os.environ['HOME'])
         with open(player_autostart_path, 'w') as file:
             file.write(xenv_presets)
